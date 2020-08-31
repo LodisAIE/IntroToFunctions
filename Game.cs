44598352a3e4657e5ce8a505995383865cf4cde5
lodis <lodisperkins44@gmail.com>
2020-08-31 08:08:43 -0500

    Added basic menu system

--- Game.cs
+++ Game.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Transactions;
 
 namespace HelloWorld
 {
+    
     class Game
     {
         //Ends the game when true
         private bool _gameOver = false;
-        
+        string playerName;
+        int playerHealth;
+        int playerAttack;
+        string playerRole;
+        int gameState;
+        char input;
+        public void OpenMainMenu()
+        {
+            
+            while(input != '1')
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome! Please Enter your name");
+                playerName = Console.ReadLine();
+                Console.WriteLine("Are you sure you want the name " + playerName + "?");
+                DisplayOptions("to accept name", "to enter new name");
+            }
+        }
+        public void DisplayOptions(string option1, string option2)
+        {
+            Console.WriteLine("Press 1 " + option1);
+            Console.WriteLine("Press 2 " + option2);
+            input = Console.ReadKey().KeyChar;
+            Console.ReadKey();
+        }
         //Run the game
         public void Run()
         {
@@ -26,13 +52,26 @@ namespace HelloWorld
         //Performed once when the game begins
         public void Start()
         {
-
+            gameState = 0;
+            input = ' ';
+            playerName = "Hero";
+            playerHealth = 100;
+            playerAttack = 20;
+            playerRole = "none";
         }
 
         //Repeated until the game ends
         public void Update()
         {
-
+            //switch (gameState)
+            //{
+            //    case 0:
+            //        {
+            //            OpenMainMenu();
+            //            break;
+            //        }
+            //}
+            OpenMainMenu();
         }
 
         //Performed once when the game ends
