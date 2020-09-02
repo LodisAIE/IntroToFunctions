7a11e228cae5eaae530f9da24261191096286b07
Lodis Perkins <lodisperkins44@gmail.com>
2020-09-02 17:26:52 -0500

    Added recursion,function modifier, and switch statement examples

--- Game.cs
+++ Game.cs
@@ -5,34 +5,27 @@ using System.Transactions;
 
 namespace HelloWorld
 {
-    
+
     class Game
     {
         bool _gameOver = false;
-        string _playerName = "";
-        
-        void RequestName()
+        string _playerName = "the player";
+        int playerHealth = 100;
+        //Type  Name       Argument/Parameter list
+        void RequestName(ref string name)
+        //body   
         {
-            //If player already has a name, return from function
-            if(_playerName != "")
-            {
-                return;
-            }
             //Initialize input value
             char input = ' ';
             //Loop until valid input is given
             while (input != '1')
             {
-                //Clear previous text
-                Console.Clear();
                 //Ask user for name
-                Console.WriteLine("Please enter your name.");
-                _playerName = Console.ReadLine();
-                //Display username 
-                Console.WriteLine("Hello " + _playerName);
+                Console.WriteLine("Please enter a new name for " + name);
+                name = Console.ReadLine();
                 //Give the user the option to change their name
-                input = GetInput("Yes", "No", "Are you sure you want the name " + _playerName + "?");
-                if(input == '2')
+                input = GetInput("Yes", "No", "Are you sure you want the name " + name + "?");
+                if (input == '2')
                 {
                     Console.WriteLine("Yeah thats a horrible name. Try again");
                 }
@@ -41,55 +34,99 @@ namespace HelloWorld
 
         void Explore()
         {
+            string petName = "the flying pitbull dragon thing";
+            string enemyName = "jeff";
             char input = ' ';
             input = GetInput("Go Left", "Go right", "You came to a cross road");
-            if(input == '1')
+            if (input == '1')
             {
-                Console.WriteLine("You decide to go left and a trap is sprung. " +
-                    "You're covered up to your chin in quick sand");
-                _gameOver = true;
+                Console.WriteLine("A group of bandits appear! It's an ambush, and they wanna take all yo money" +
+                    "Then a " + petName + "out, and shoots fire! All the bandits are dead. Except for one. He's just in pain." +
+                    "It seems like the flying pitbull dragon thing wants to join you on your journey. What should you name it?");
+                RequestName(ref enemyName);
+                Console.WriteLine("New name is " + petName);
+                Console.ReadKey();
             }
-            else if(input == '2')
+            else if (input == '2')
             {
-                Console.WriteLine("You continue your journey and head towards Portlad");
+                Console.WriteLine("You continue your journey and head towards Portland");
                 Console.ReadKey();
             }
             Console.Clear();
-            Console.WriteLine("Start fight encounter");
-            int enemy = 180;
-            int playerHealth = 120;
-            _gameOver = StartBattle(ref playerHealth, enemy);
+            Console.WriteLine("The last bandit rushes towards you in rage! 'You killed my wife and kids!' He screams.");
+
+            int enemyHealth = 75;
+            _gameOver = StartBattle(ref playerHealth);
         }
 
-        bool StartBattle(ref int playerHealth, int enemyHealth)
+        void EnterRoom(int roomNumber)
         {
-            char input = ' ';
-            while(playerHealth > 0 && enemyHealth > 0)
+            string exitMessage = "";
+            switch (roomNumber)
             {
+                case 0:
+                    {
+                        exitMessage = "You depart from Castle Grayskull";
+                        Console.WriteLine("Before you stands the entrance to Castle Grayskull");
+                        Console.WriteLine("A monster appears to block your path");
+                        StartBattle(ref playerHealth);
+                        break;
+                    }
+                case 1:
+                case 2:
+                    {
+                        exitMessage = "You leave the kitchen";
+                        Console.WriteLine("You enter the castle's kitchen. There's knives on the ground, rats everywhere, and moldy chicken");
+                        break;
+                    }
+
+                default:
+                    {
+                        exitMessage = "You left the hallway";
+                        Console.WriteLine("You enter a seemingly never ending hallway");
+                        break;
+                    }
+            }
+            Console.WriteLine("You are in room " + roomNumber);
+            char input = ' ';
+            input = GetInput("Go forward", "Go back", "Which direction would you like to go?");
+            if(input == '1')
+            {
+                EnterRoom(roomNumber+1);
+            }
+
+            Console.WriteLine(exitMessage);
+        }
+
+        bool StartBattle(ref int character1Health, int character2Health = 100)
+        {
+            //initialize the input variable
+            char input = ' ';
+            //Create battle loop. Loops until the player or enemy is dead
+            while (character1Health > 0 && character2Health > 0)
+            {
+                Console.WriteLine("Character 1 Health: " + character1Health);
+                Console.WriteLine("Character 2 Health: " + character2Health);
+                //Get input from player
                 input = GetInput("Attack", "Defend", "What will you do?");
-                if(input == '1')
+                //If input is 1 then the player attacks the enemy
+                if (input == '1')
                 {
-                    enemyHealth -= 10;
-                    Console.WriteLine("You did 10 damage to the enemy");
+                    character2Health -= 10;
+                    Console.WriteLine("You attacked and did 10 damage");
                 }
-                else if(input == '2')
+                //if input is 2 then the player blocked the enemy's attack
+                else if (input == '2')
                 {
-                    Console.WriteLine("You blocked the enemies attack");
+                    Console.WriteLine("You blocked the enemy's attack!");
                     Console.ReadKey();
                     continue;
                 }
-                playerHealth -= 20;
-                Console.WriteLine("The enemy did 20 damage to you");
+                character1Health -= 20;
+                Console.WriteLine("The enemy attacked and did 20 damage!");
                 Console.ReadKey();
             }
-            if(playerHealth <= 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return character1Health <= 0;
         }
 
         void ViewStats()
@@ -103,11 +140,11 @@ namespace HelloWorld
 
         char GetInput(string option1, string option2, string query)
         {
-            
+
             //Initialize input value
             char input = ' ';
             //Loop until a valid input is received
-            while(input != '1' && input != '2')
+            while (input != '1' && input != '2')
             {
                 Console.WriteLine(query);
                 //Display options
@@ -119,7 +156,7 @@ namespace HelloWorld
                 input = Console.ReadKey().KeyChar;
                 Console.WriteLine();
                 //If the player input 3, call the view stats function
-                if(input == '3')
+                if (input == '3')
                 {
                     ViewStats();
                 }
@@ -132,8 +169,8 @@ namespace HelloWorld
         public void Run()
         {
             Start();
-            
-            while(_gameOver == false)
+
+            while (_gameOver == false)
             {
                 Update();
             }
@@ -153,8 +190,8 @@ namespace HelloWorld
         //Used for all game logic that will repeat
         public void Update()
         {
-            RequestName();
-            Explore();
+            //RequestName(ref _playerName);
+            EnterRoom(0);
         }
 
         //Performed once when the game ends
